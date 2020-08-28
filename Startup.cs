524c602678fcecb0bc06a732e97f501909209643
Oriol Burgaya <oriolbv@gmail.com>
2020-08-28 18:41:17 +0200

    Startup modified

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CarRentalAPI.Models;
 
 namespace CarRentalAPI
 {
@@ -25,6 +27,8 @@ namespace CarRentalAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CarRentalContext>(opt =>
+               opt.UseInMemoryDatabase("CarRental"));
             services.AddControllers();
         }
 
